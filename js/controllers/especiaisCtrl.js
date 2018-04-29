b393b54fa6a73f7bb65e80de18dca1b21f7b5b83
Mikael italo <mikael.italo.35@gmail.com>
2018-04-28 21:35:33 -0300

    img favorite/array tabaca/array especiais

--- js/controllers/especiaisCtrl.js
+++ js/controllers/especiaisCtrl.js
@@ -1,7 +1,16 @@
 app.controller('especiaisCtrl', function($scope, $stateParams){
 
 
- 
+ $scope.especiaisTipo = [
+	{'tipo_especiais': 'Gelo'},
+	{'tipo_especiais': 'Carvão'},
+	{'tipo_especiais': 'Palheiro'},
+	{'tipo_especiais': 'Essências'},
+	{'tipo_especiais': 'Carvão'},
+	{'tipo_especiais': 'Sedas'},
+	{'tipo_especiais': 'Isqueiro'}
+
+	];
 
 
 
